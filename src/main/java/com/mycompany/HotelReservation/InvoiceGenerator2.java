8209d4464a753872d9264e06bd390bc747143590
YadavGulshan <yadavgulshan542@gmail.com>
2021-11-29 12:40:03 +0530

    Added the constructor and defined the req vars

--- src/main/java/com/mycompany/HotelReservation/InvoiceGenerator2.java
+++ src/main/java/com/mycompany/HotelReservation/InvoiceGenerator2.java
@@ -1,33 +1,33 @@
 package com.mycompany.HotelReservation;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-
 import com.itextpdf.text.Document;
-import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.Paragraph;
-import com.itextpdf.text.pdf.PdfWriter;
-//
+
+
 public class InvoiceGenerator2 {
-    public static void main(String args[]) {
-//created PDF document instance
-        Document doc = new Document();
-        try {
-//generate a PDF at the specified location
-            PdfWriter writer = PdfWriter.getInstance(doc, new FileOutputStream("out/invoices/Motivation.pdf"));
-            System.out.println("PDF created.");
-//opens the PDF
-            doc.open();
-//adds paragraph to the PDF file
-            doc.add(new Paragraph("If you're offered a seat on a rocket ship, don't ask what seat! Just get on."));
-//close the PDF file
-            doc.close();
-//closes the writer
-            writer.close();
-        } catch (DocumentException e) {
-            e.printStackTrace();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+    int Userid;
+    String Username;
+    int UserPhone;
+    String UserEmail;
+
+
+    // Room Details var
+    int RoomNumber;
+    String RoomType;
+    String BedType;
+    int CalculatedPrice;
+
+    // Constructor.
+    InvoiceGenerator2(int id, String Name, int Phone,String Email, int Room, String Type, String Bed, int Price ){
+        Userid = id;
+        Username = Name;
+        UserEmail = Email;
+        RoomNumber = Room;
+        RoomType = Type;
+        BedType = Bed;
+        CalculatedPrice = Price;
     }
+    public static void addMetaData(Document document){
+
+    }
+
 }
