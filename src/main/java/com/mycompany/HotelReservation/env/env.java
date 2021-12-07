dab604e7390d59c0266c2c5109f4fcf96c2e5877
YadavGulshan <yadavgulshan542@gmail.com>
2021-12-07 20:15:02 +0530

    removed the email addr

--- src/main/java/com/mycompany/HotelReservation/env/env.java
+++ src/main/java/com/mycompany/HotelReservation/env/env.java
@@ -1,6 +1,6 @@
 package com.mycompany.HotelReservation.env;
 
 public class env {
-    protected static final String Email = "gulshanprojects@gmail.com";
+    protected static final String Email = "YOUR_EMAIL";
     protected static final String Password = "YOUR_PASSWORD";
 }
