a0da3939482e46a96c4b70cab5b6a952efaea6b8
YadavGulshan <yadavgulshan542@gmail.com>
2021-12-07 20:14:08 +0530

    removed the api key

--- src/main/java/com/mycompany/HotelReservation/env/env.java
+++ src/main/java/com/mycompany/HotelReservation/env/env.java
@@ -2,5 +2,5 @@ package com.mycompany.HotelReservation.env;
 
 public class env {
     protected static final String Email = "gulshanprojects@gmail.com";
-    protected static final String Password = "zecansstrnbblopo";
+    protected static final String Password = "YOUR_PASSWORD";
 }
